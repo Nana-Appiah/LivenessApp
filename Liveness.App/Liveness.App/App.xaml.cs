5e3a5d01242a162c1e65f6ddedc32e740ab9b398
Nana-Appiah <nappiah@panafricansl.com>
2022-06-08 11:34:08 +0100

    Initial commit for Xamarin.Forms NIA authentication Project

---
+++ Liveness.App/Liveness.App/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Liveness.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //chaning the main page from the default to ContentPageExample
+            //MainPage = new MainPage();
+            MainPage = new ContentPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
